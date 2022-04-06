a2dfd72af873fab23bafc68cb7bab8d6c918ba87
Rayleigh Rozier <rayleighrozier@gmail.com>
2022-04-06 14:19:45 -0400

    work on connecting supabase

--- group-redux/src/actions/supabase.js
+++ group-redux/src/actions/supabase.js
@@ -1,12 +1,29 @@
 import { createClient } from "@supabase/supabase-js";
 let supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMDE0NTg3OCwiZXhwIjoxOTM1NzIxODc4fQ.0qra9FQYuKCkgMteZ0ZAe2wrMx2v1IFGwsU60Oi4KwY";
-let supabaseUrl = "https://mxakcphctxajogkuteth.supabase.co";
+let supabaseUrl = "https://mxakcphctxajogkuteth.supabase.com";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-const { user, session, error } = await supabase.auth.signIn({
-  email: "example@email.com",
-  password: "example-password",
-});
+const userSignUp = async (email, password) => {
+  const { user, session, error } = await supabase.auth.signUp({
+    email: email,
+    password: password,
+  });
+  if (error) {
+    return error;
+  }
+  return user;
+};
 
-export { supabase, supabaseUrl, supabaseKey };
+const getExampleUser = async () => {
+  const { user, session, error } = await supabase.auth.signIn({
+    email: "hi@hi.com",
+    password: "1234",
+  });
+  if (error) {
+    return error;
+  }
+  return user;
+};
+
+export { supabase, supabaseUrl, supabaseKey, userSignUp, getExampleUser };
