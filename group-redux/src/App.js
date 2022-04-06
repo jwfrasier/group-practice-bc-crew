46f86ba17e739d57e34ce351da7c4670b6ac5bc1
ciaracloud <ciara_cloud@yahoo.com>
2022-04-06 14:47:39 -0400

    adds navbar

--- group-redux/src/App.js
+++ group-redux/src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   // comment
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route>
             <Route path="/" element={<Home />} />
--- group-redux/src/components/Navbar/Navbar.js
+++ group-redux/src/components/Navbar/Navbar.js
@@ -7,10 +7,10 @@ export default function Navbar() {
     <div className="navbar">
       <h2 className="logo">Breakfast Club Crew 🥣 </h2>
       <div className="navbarLinks">
-        <a className="link" href="">
+        <a className="link" href="http://localhost:3000/">
           Login
         </a>
-        <a className="link" href="">
+        <a className="link" href="http://localhost:3000/dashboard">
           Dashboard
         </a>
       </div>
