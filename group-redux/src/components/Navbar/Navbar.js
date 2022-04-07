53acbeaeffacf9542c7333bb01b70298cb001b4b
Rayleigh Rozier <rayleighrozier@gmail.com>
2022-04-07 14:27:44 -0400

    navbar css

--- group-redux/src/components/Navbar/Navbar.js
+++ group-redux/src/components/Navbar/Navbar.js
@@ -1,17 +1,27 @@
 import "./Navbar.css";
-
 import React from "react";
+import LoginIcon from "../Icons/LoginIcon";
+import LogoutIcon from "../Icons/LogoutIcon";
+import DashboardIcon from "../Icons/DashboardIcon";
 
 export default function Navbar() {
   return (
     <div className="navbar">
       <h2 className="logo">Breakfast Club Crew 🥣 </h2>
       <div className="navbarLinks">
-        <a className="link" href="http://localhost:3000/">
-          Login
-        </a>
         <a className="link" href="http://localhost:3000/dashboard">
           Dashboard
+          <DashboardIcon />
+        </a>
+
+        <a className="link" href="http://localhost:3000/login">
+          Login
+          <LoginIcon />
+        </a>
+
+        <a className="link" href="http://localhost:3000/">
+          Logout
+          <LogoutIcon />
         </a>
       </div>
     </div>
