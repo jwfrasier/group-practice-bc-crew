774cc1e7f0c8a1f4715f574848306cfbe319b3ad
Rayleigh Rozier <rayleighrozier@gmail.com>
2022-04-07 15:36:39 -0400

    responsive navbar

--- group-redux/src/components/Navbar/Navbar.js
+++ group-redux/src/components/Navbar/Navbar.js
@@ -7,20 +7,23 @@ import DashboardIcon from "../Icons/DashboardIcon";
 export default function Navbar() {
   return (
     <div className="navbar">
-      <h2 className="logo">Breakfast Club Crew 🥣 </h2>
+      <div className="logo">
+        <h2 className="breakfastClub">Breakfast Club Crew</h2>
+        <p className="bowl">🥣</p>
+      </div>
       <div className="navbarLinks">
         <a className="link" href="http://localhost:3000/dashboard">
-          Dashboard
+          <p className="linkTitle">Dashboard</p>
           <DashboardIcon />
         </a>
 
         <a className="link" href="http://localhost:3000/login">
-          Login
+          <p className="linkTitle">Login</p>
           <LoginIcon />
         </a>
 
         <a className="link" href="http://localhost:3000/">
-          Logout
+          <p className="linkTitle">Logout</p>
           <LogoutIcon />
         </a>
       </div>
