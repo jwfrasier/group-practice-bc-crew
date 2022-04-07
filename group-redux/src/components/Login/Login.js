ee70cc07833745fc2f48464125d02bd6b19313bc
Rayleigh Rozier <rayleighrozier@gmail.com>
2022-04-06 20:18:15 -0400

    redirects to dashboard page after login

--- group-redux/src/components/Login/Login.js
+++ group-redux/src/components/Login/Login.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Login.css";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { useSelector, useDispatch } from "react-redux";
 import { userSignIn } from "../../actions/supabase";
 
 export default function Login() {
   const loginField = useSelector((state) => state.user.loginField);
   const loginPassword = useSelector((state) => state.user.loginPassword);
+  const loggedIn = useSelector((state) => state.user.loggedIn);
   const dispatch = useDispatch();
   const changeInputField = (e) => {
     dispatch({
@@ -13,10 +17,24 @@ export default function Login() {
       payload: e.target.value,
     });
   };
+  //login function
   const signIn = async () => {
     let data = await userSignIn(loginField, loginPassword);
     console.log(data);
+    dispatch({
+      type: "SET_LOGGED_IN",
+    });
+    console.log(loggedIn);
   };
+
+  // redirects to dashboard once logged in
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (loggedIn) {
+      navigate("/dashboard");
+    }
+  }, [loggedIn]);
+
   return (
     <div className="loginContainer">
       <h1>Login</h1>
--- group-redux/src/reducers/userReducer.js
+++ group-redux/src/reducers/userReducer.js
@@ -2,10 +2,12 @@ import {
   SET_USER,
   SET_LOGIN_PASSWORD,
   SET_LOGIN_EMAIL,
+  SET_LOGGED_IN,
 } from "../action-types/index";
 const initialState = {
   loginField: "",
   loginPassword: "",
+  loggedIn: false,
 };
 
 const user = (state = initialState, action) => {
@@ -16,6 +18,9 @@ const user = (state = initialState, action) => {
       return (state = { ...state, loginField: action.payload });
     case SET_LOGIN_PASSWORD:
       return (state = { ...state, loginPassword: action.payload });
+    case SET_LOGGED_IN: {
+      return (state = { ...state, loggedIn: true });
+    }
     default:
       return state;
   }
